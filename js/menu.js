e67be1202d0f07eead3d403186b45b5347f22c7d
Vikulov Andrey <vikulov.prog@gmail.com>
2021-11-06 13:04:46 +0400

    1) Добавлен скрипт на страницу restaurant.html для обработки модальных окон, сам же файл переименован в auth.js 2) Добавлен скрипт с обработкой json данных partners.js для главной страницы 3) Добавлен скрипт с обработкой json данных menu.js для вспомогательной  страницы 4) Добавлена возможность обработки диномичных данных - возможность подстановки переменной со значением категории - для помледущей обработки категорий у разных карточек в menu.js 5) добавлена поддержка https://console.firebase.google.com c тестовой базой 6) добавлены фавиконки - раздражала ошибка в консоле:) 7) обновлена обложка для гита(уменьшен размер) 8) добавлен перебор елементов из массива данных полученнных из базы с проверкой на длину и если 0 на существование

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,16 @@
 const restaurant = 'food-band'
 // функционально обробатываем данные из базы(формат json)
 const renderItems = (data) => {
-    console.log(data)
+    if(data.length > 0 ){
+        data.forEach((el) => {{
+            console.log(el);
+
+        }})
+    }else{
+        if(data){
+            console.log(data);
+        }
+    }
 }
 // получаем тестовые данные из firebase.google.com
 fetch(`https://test-b0177-default-rtdb.firebaseio.com/db/${restaurant}.json`)
--- js/partners.js
+++ js/partners.js
@@ -1,6 +1,17 @@
 // функционально обробатываем данные из базы(формат json)
 const renderItems = (data) => {
-    console.log(data)
+    if(data.length > 0 ){
+        data.forEach((el) => {{
+            console.log(el);
+
+        }})
+    }else{
+        if(data){
+            console.log(data);
+        }
+    }
+
+
 }
 
 fetch('https://test-b0177-default-rtdb.firebaseio.com/db/partners.json')
